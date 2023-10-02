ee305b86dfa7becaa3f5d0ded592e63b43524b72
pawanjs <hello.pawanjs@gmail.com>
2023-10-02 19:26:54 +0530

    created navigation container

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {StyleSheet, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <>
+    <NavigationContainer>
       <Text style={styles.text}>Hola</Text>
-    </>
+    </NavigationContainer>
   );
 }
 
